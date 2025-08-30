38d5119a723ed8131bfb46eec7c2ee1fcaab9f51
Lucas <lucas.paaz5@gmail.com>
2025-08-29 21:58:20 -0300

    style(router): remove comentários sobrando

--- app/testes/test_app.py
+++ app/testes/test_app.py
@@ -1,13 +1,10 @@
-# tests/test_rabbit.py
-import pytest
 import json
-from unittest.mock import AsyncMock, patch
-from fastapi.testclient import TestClient
-from router.rabbit import Rabbit, NOTIFICACOES
-from schemas.rabbit_schemas import Notificar
+import pytest
 from fastapi import FastAPI
+from router.rabbit import Rabbit
+from fastapi.testclient import TestClient
+from unittest.mock import AsyncMock, patch
 
-# cria app só pra teste
 app = FastAPI()
 app.include_router(Rabbit().router, prefix="")
 
@@ -16,7 +13,7 @@ client = TestClient(app)
 @pytest.mark.asyncio
 async def test_notificar_publica_mensagem():
     data = {
-        "mensagemId": "123",
+        "mensagemId": None,
         "conteudoMensagem": "teste",
         "tipoNotificacao": "EMAIL"
     }
@@ -28,22 +25,17 @@ async def test_notificar_publica_mensagem():
     with patch("router.rabbit.aio_pika.connect_robust", return_value=fake_conn):
         resp = client.post("/api/notificar", json=data)
 
-    # resposta da API
     assert resp.status_code == 202
     body = resp.json()
     assert body["status"] == "ACCEPTED"
     assert "traceId" in body
 
-    # verifica se publicou no exchange
     fake_channel.default_exchange.publish.assert_awaited()
     args, kwargs = fake_channel.default_exchange.publish.call_args
 
-    # verifica que payload bate
     sent_msg = args[0]
     payload = json.loads(sent_msg.body.decode())
-    assert payload["mensagemId"] == "123"
     assert payload["conteudoMensagem"] == "teste"
     assert payload["tipoNotificacao"] == "EMAIL"
 
-    # verifica routing key usada
-    assert kwargs["routing_key"]  # bate com QUEUE_NAME do env
+    assert kwargs["routing_key"]  
